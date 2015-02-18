2725071d7e6cf132bf03b4cf3fd327754b95a93c
CarmenGraus <carmengraus95@gmail.com>
2015-02-18 17:24:05 +0100

    #479 y #480
    Terminado, programación andar y correr

---
+++ TheFinalUnity/Assets/Scripts/generadorScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class generadorScript : MonoBehaviour {
+
+	public GameObject[] pinguino;
+	public float tiempoespera = 3;
+	float timer = 0;
+	Vector3 posicion;
+	// Use this for initialization
+	void Start () {
+		posicion = transform.position; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > timer) {
+			var altura = Random.Range(-1.5f,1.5f);
+			var tipopinguino = Random.Range (0,pinguino.Length);
+			
+			var nuevopinguino = (GameObject)Instantiate(
+				pinguino[tipopinguino],
+				new Vector3(posicion.x,posicion.y+altura,posicion.z), 
+				transform.rotation);
+			
+			Destroy (nuevopinguino,tiempoespera*3);
+			
+			timer = Time.time+tiempoespera;
+		}
+	}
+}
---
+++ TheFinalUnity/Assets/Scripts/limiteVelocidad.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class limiteVelocidad : MonoBehaviour {
+	public float limitey = -1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (rigidbody2D.velocity.y);
+		if (rigidbody2D.velocity.y < limitey) {
+			rigidbody2D.velocity = new Vector2 (0,limitey);
+				}
+	}
+}
