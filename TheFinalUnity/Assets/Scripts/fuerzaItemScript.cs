ab0c2ec997bc1b763267b70ae36e3d7c4ba62ba6
CarmenGraus <carmengraus95@gmail.com>
2015-03-05 19:13:24 +0100

    #cosas

---
+++ TheFinalUnity/Assets/Scripts/fuerzaItemScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fuerzaItemScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnMouseDown(){
+		salud = salud - 300;
+		animation.SetInteger ("vida",salud);
+}
+}
---
+++ TheFinalUnity/Assets/Scripts/tiempoItemScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tiempoScript : MonoBehaviour {
+	public float limitey = -0.01f;
+	// Use this for initialization
+	void Start () {
+
+		 GameObject.FindWithTag("animales");
+
+	}
+	void OnMouseDown(){
+		//Debug.Log (rigidbody2D.velocity.y);
+		
+		Destroy (gameObject);
+		
+	}
+	// Update is called once per frame
+	void OnDestroy() {
+		//Debug.Log (rigidbody2D.velocity.y);
+		if (rigidbody2D.velocity.y < limitey) {
+			rigidbody2D.velocity = new Vector2 (0,limitey);
+		}
+	}
+
+	}
+
